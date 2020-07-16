eb6e5879e4c5a8b54a5ff6f1463c99ca37828b6a
tamimvaiya <tamimvaiya.123@gmail.com>
2020-07-16 17:01:02 +0600

    fixed

--- randomNum.js
+++ randomNum.js
@@ -1,7 +1,6 @@
 function random(n){
-  var randomNumber = Math.round((Math.random()*n)+1); 
+  var randomNumber = Math.floor((Math.random()*n)+1); 
   return randomNumber;
 }
 var result = random(6);
 console.log(result);
-//sometimes is call 7, but i will fix it soon!
